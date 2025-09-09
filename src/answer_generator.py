b9f3838c34561083e3dec4ddaa15800003c22492
Chrome1278 <chromechromovich@gmail.com>
2025-09-09 22:57:54 +0300

    added start version of RAG realisation

---
+++ src/answer_generator.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from transformers import pipeline
+
+from search_engine import SearchIndex
+
+
+
+SYSTEM_PROMPT = """Answer the question based only on the following information in the 'Context' section which contains a description of the films.
+    If the text does not answer the question from the context, say: 'I do not have enough information to answer the question.'
+"""
+
+
+
+class AnswerGenerator:
+
+    def __init__(self, model_name: str, ):
+
+        self.pipe = pipeline("text-generation", model=model_name)
+
+        self.search_index = SearchIndex(
+            chunks_path='./data/chunks_metadata.pkl',
+            index_path='./data/movies_info.index'
+        )
+
+        self.movies_df = pd.read_csv('./data/movies_data.csv')
+
+
+    def generate_movie_answer(self, question, context_chunks):
+
+        finded_movies = []
+        for i, chunk in enumerate(context_chunks):
+            movie_info = self.movies_df['movie_text'].iloc[chunk['source_index']]
+            finded_movies.append(f"## Info about a movie number {i+1}.\n{movie_info}")
+        movies_context = '\n'.join(finded_movies)
+
+        user_prompt = f"""
+        # Context:
+        {movies_context}
+        # Question: 
+        {question}
+        # Answer:
+        """
+
+        messages = [dict(role="system", content=SYSTEM_PROMPT),
+                    dict(role="user", content=user_prompt)]
+
+        result = self.pipe(messages)
+        answer = result[0]['generated_text'][-1]['content']
+
+        return answer
+
+    def __call__(self, query: str) -> str:
+        context_chunks = self.search_index.search_by_query(query, top_k=3)
+        result = self.generate_movie_answer(query, context_chunks)
+        return result
+
+
+if __name__ == "__main__":
+
+    answer_generator = AnswerGenerator(model_name="Gensyn/Qwen2.5-0.5B-Instruct")
+
+    question = "Tell me about a biographical movie with a hunting expedition"
+    answer = answer_generator(question)
+
+    print(f"Question: {question}")
+    print(f"Answer: {answer}")
---
+++ src/search_engine.py
@@ -0,0 +1,62 @@
+import pickle
+from sentence_transformers import SentenceTransformer
+import faiss
+
+
+class SearchIndex:
+
+    def __init__(self, chunks_path: str, index_path: str,
+                 encoder_model: str = "multi-qa-mpnet-base-dot-v1"):
+        self.index = faiss.read_index(index_path)
+        with open(chunks_path, 'rb') as f:
+            chunks_metadata = pickle.load(f)
+        self.chunks = chunks_metadata['chunks']
+        self.source_chunk_indexes = chunks_metadata['source_index']
+
+        self.model = SentenceTransformer(encoder_model)
+
+    def search_by_query(self, query, top_k=3):
+        """
+        Поиск фильмов с возможностью фильтрации по году и жанру
+        """
+        query_embedding = self.model.encode([query])
+
+        distances, indices = self.index.search(query_embedding, top_k)
+
+        results = []
+        unique_indices = set()  # Множество для отслеживания уже добавленных source_index
+
+        for i in range(len(indices[0])):
+            source_idx = self.source_chunk_indexes[indices[0][i]]
+
+            if source_idx in unique_indices:
+                continue
+
+            results.append({
+                'chunk': self.chunks[indices[0][i]],
+                'source_index': source_idx,
+                'score': distances[0][i]
+            })
+            unique_indices.add(source_idx)
+
+            if len(results) >= top_k:
+                break
+
+        return results[:top_k]
+
+# Тестирование поиска
+
+if __name__ == "__main__":
+    search_index = SearchIndex(
+        chunks_path='./data/chunks_metadata.pkl',
+        index_path='./data/movies_info.index'
+    )
+    results = search_index.search_by_query(
+        "Daniel Boone daughter befriends an Indian maiden",
+        top_k=5
+    )
+    for i, result in enumerate(results):
+        print(f"\nResult {i+1}:")
+        print(f"INDEX: {result['source_index']}")
+        print(f"Relevance: {result['score']:.3f}")
+        print(f"Content:\n {result['chunk']}...")
